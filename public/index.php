24b2f4289e83c2cc58f6c25566a529fc145d80a8
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-11-01 16:35:28 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,29 +1,15 @@
-<html>
-<head><title>postgresql_view.php</title></head>
-<body>
+
+
 <?php
-
-  // PostgreSQL 接続をオープンする
-  $dbconn = pg_connect("port=5432 user=uycydaxapjtpqc password=c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943
- dbname=dfjra63p39v988");
-  // SQL文の作成
-  $sql = "SELECT product_no, product_name, price FROM products WHERE product_name = $1";
-  // SQLとパラメータを分割して実行
-  $result = pg_query_params($dbconn, $sql, array($_POST['product_name']));
-  
-  // SQL実行結果の行数を返す
-  $cnt = pg_numrows($result);
-  print("$cnt 件ヒットしました。<br />");
-  
-  for ($i = 0; $i < $cnt; $i++) {
-    // 実行結果のi行目の行情報を取り出す
-    $row = pg_fetch_row($result, $i);
-    print("product_no：$row[0] product_name：$row[1] price：$row[2]");
-  }
-
-  // PostgreSQL 接続をクローズする
-  pg_close($dbconn);
-
+$con = pg_connect("dbname=postgres user=postgres password=postgres");
+$contents = $_POST['contents'];
+if ($contents) {
+  $contents = pg_escape_string(htmlspecialchars($contents));
+  pg_query($con, "insert into pgbbs1 values('$contents')");
+}
+$rs = pg_query($con, "select contents from pgbbs1");
+while ($row = pg_fetch_array($rs)) {
+  print $row['contents'] . "<br>\n";
+}
+pg_close($con);
 ?>
-</body>
-</html>
