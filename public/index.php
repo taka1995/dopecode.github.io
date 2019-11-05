c75d82f2df0b6a618664a8226caf597aacdb9b72
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-11-05 13:48:04 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
   // PostgreSQL 接続をオープンする
   $dbconn = pg_connect("port=5432 user=uycydaxapjtpqc password=c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943 dbname=dfjra63p39v988");
   // SQL文の作成
-  $sql = "SELECT product_no, product_name, price FROM products WHERE product_name = $1";
+  $sql = "SELECT FirstName, LastName, Email FROM lead;
   // SQLとパラメータを分割して実行
   $result = pg_query_params($dbconn, $sql, array($_POST['product_name']));
   
@@ -18,7 +18,7 @@
   for ($i = 0; $i < $cnt; $i++) {
     // 実行結果のi行目の行情報を取り出す
     $row = pg_fetch_row($result, $i);
-    print("product_no：$row[0] product_name：$row[1] price：$row[2]");
+    print("FirstName：$row[0] LastName：$row[1] Email：$row[2]");
   }
 
   // PostgreSQL 接続をクローズする
