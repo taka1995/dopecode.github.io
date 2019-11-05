bc80e6b2880cfa90c6136c29f472c98727e42523
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-11-05 12:30:39 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,31 +1,29 @@
 
+<html>
+<head><title>index.php</title></head>
+<body>
 <?php
 
-$hostname = "ec2-23-21-94-99.compute-1.amazonaws.com";
-$uname = "uycydaxapjtpqc";
-$upass = "c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943";
-$dbname = "dfjra63p39v988";
-$tblname = "lead";
+  // PostgreSQL 接続をオープンする
+  $dbconn = pg_connect("port=5432 user=uycydaxapjtpqc password=c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943 dbname=dfjra63p39v988");
+  // SQL文の作成
+  $sql = "SELECT product_no, product_name, price FROM products WHERE product_name = $1";
+  // SQLとパラメータを分割して実行
+  $result = pg_query_params($dbconn, $sql, array($_POST['product_name']));
+  
+  // SQL実行結果の行数を返す
+  $cnt = pg_numrows($result);
+  print("$cnt 件ヒットしました。<br />");
+  
+  for ($i = 0; $i < $cnt; $i++) {
+    // 実行結果のi行目の行情報を取り出す
+    $row = pg_fetch_row($result, $i);
+    print("product_no：$row[0] product_name：$row[1] price：$row[2]");
+  }
 
-//PostgreSQL に接続する。
-$connect_string = "host={$hostname} dbname={$dbname}";
-$connect_string .= " port=5432 user={$uname} password={$upass}";
-
-if( !$res_dbcon = pg_connect( $connect_string ) ){
-	print "PostgreSQL への接続に失敗しました。";
-	exit;
-}
-
-//ＳＱＬ文を実行し、結果のリソースを取得。
-$res_result = pg_query( $res_dbcon, "SELECT * from {$tblname}" );
-
-//問い合わせ結果を表示。
-while( $row = pg_fetch_array( $res_result, NULL, PGSQL_ASSOC ) ){
-	var_dump( $row );
-}
-
-//PostgreSQL への接続を切断する。
-//この処理を行わなくてもプログラム終了時に自動的に切断される。
-pg_close( $res_dbcon );
+  // PostgreSQL 接続をクローズする
+  pg_close($dbconn);
 
 ?>
+</body>
+</html>
