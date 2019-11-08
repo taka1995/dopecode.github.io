374aa8edb6dc7c9d7343f8ea3ff61c22aef3ae3b
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-11-08 16:43:54 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,22 @@
 <?php
 
   // PostgreSQL 接続をオープンする
-  $dbconn = pg_connect("port=5432 user=uycydaxapjtpqc password=c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943 dbname=dfjra63p39v988");
+  $conn = pg_connect("port=5432 user=uycydaxapjtpqc password=c57ee137fc8bff8965bae9d035872dd33607a849a8345c23c92e3eb286a7b943 dbname=dfjra63p39v988");
   // SQL文の作成
- 
-print_r($_POST);
-  // PostgreSQL 接続をクローズする
-  pg_close($dbconn);
+ $link = pg_connect($conn);
+if (!$link) {
+    die('接続失敗です。'.pg_last_error());
+}
+
+print('接続に成功しました。<br>');
+
+// PostgreSQLに対する処理
+
+$close_flag = pg_close($link);
+
+if ($close_flag){
+    print('切断に成功しました。<br>');
+}
 
 ?>
 </body>
